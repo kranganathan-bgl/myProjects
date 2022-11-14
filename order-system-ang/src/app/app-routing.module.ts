e2fbb4610dcc058834cf72618c1f5541bdbf8ecf
Kan <kranganathan@bglcorp.com.au>
2022-11-14 16:09:39 +1100

    order_system commit

--- order-system-ang/src/app/app-routing.module.ts
+++ order-system-ang/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {OrdersComponent} from "./components/orders/orders.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: OrdersComponent},
+  {path: 'orders', component: OrdersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- order-system-ang/src/app/app.module.ts
+++ order-system-ang/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './components/orders/orders.component';
+import { OrderComponent } from './components/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrdersComponent
+    OrdersComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
